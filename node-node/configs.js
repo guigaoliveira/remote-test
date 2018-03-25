623f4376dec56a0dd9ebd6338f2ad99e28c886b5
guiga <gmbo182@hotmail.com>
2018-03-25 15:20:23 -0300

    fixs

--- node-node/configs.js
+++ node-node/configs.js
@@ -2,14 +2,14 @@ exports.mqtt = {
   qos: 0,
   limit: 10,
   payloadSizes: [5, 20],
-  url: 'mqtt://localhost:1883',
+  url: 'mqtt://auth.saiot.ect.ufrn.br:8001',
 }
 
 exports.ws = {
   limit: 10,
   payloadSizes: [5, 20],
   port: 8001,
-  url: 'ws://localhost:8001',
+  url: 'ws://auth.saiot.ect.ufrn.br:8000',
 }
 
 exports.express = {
